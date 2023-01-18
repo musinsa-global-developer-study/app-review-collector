1c03983d706d070f26f82b8b213ab40fa9f5b6ee
노웅래 <wlroh.paul@gmail.com>
2023-01-18 20:01:47 +0900

    chore: 프로젝트 초기 설정

---
+++ src/main/java/com/mss/review/ReviewCollectorApplication.java
@@ -0,0 +1,13 @@
+package com.mss.review;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewCollectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewCollectorApplication.class, args);
+	}
+
+}
