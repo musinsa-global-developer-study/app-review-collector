6e6be29bee67678aed37fa65bc65051d15a816b0
노웅래 <wlroh.paul@gmail.com>
2023-01-18 20:22:55 +0900

    feat: RssReviewFeed를 구현하라

---
+++ src/main/java/com/mss/review/domain/RssReviewFeed.java
@@ -0,0 +1,19 @@
+package com.mss.review.domain;
+
+import java.time.LocalDateTime;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class RssReviewFeed {
+
+    private String authorName;
+    private String title;
+    private String content;
+    private double rating;
+    private String appVersion;
+    private LocalDateTime registerDate;
+
+}
